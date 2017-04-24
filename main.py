afbebc9f23e3f8f5aad34c95063c14a9b5d2ba40
Collin James <jamesc2@oregonstate.edu>
2017-04-23 21:52:04 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from flask import Flask
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
+
+
+@app.route('/')
+def hello():
+    timenow = datetime.today()
+    timestr = timenow.strftime("%A, %B %d, %Y, %I:%M:%S%p")
+    """Return current time."""
+    # return str(timenow)
+    return timestr
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Return a custom 404 error."""
+    return 'Sorry, nothing at this URL.', 404
